8b099853fdfe0bceb148a94261cb6a810d9f0984
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2013-05-16 14:22:25 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='django-cadoshop',
+    version='0.1.0',
+    description='Cado Shop',
+    author='Frank Wawrzak (CadoSolutions)',
+    author_email='frank.wawrzak@cadosolutions.com',
+    url='https://github.com/fsw/django-cadolib',
+    download_url='git://github.com/fsw/django-cadolib.git',
+    packages=['cadoshop'],
+    install_requires=[
+        'cadolib',
+		'plata',
+    ],
+)
